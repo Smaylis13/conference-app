062c835ef5fbb56a820a14bdc4c9193f85fa434c
Ismail GACHA <igacha664@gmail.com>
2017-11-13 17:50:29 +0100

    etape routeur en cours

--- src/index.js
+++ src/index.js
@@ -11,12 +11,30 @@ import layout from "./layout/index"
 // intégration JQuery
 window.$ = window.jQuery = require('jquery');
 
-//const talkService = new TalkService()
+const talkService = new TalkService()
 
 
 const tabSpeakers = talkService.findAllSpeakers()
 let i = 0
 tabSpeakers.then((data) => {
     for(i in data)
-        console.log(data[i].firstname + " "+data[i].lastname)
+        console.log(data[i].firstname)
  } )
+
+ // Etape 4 - Routeur 
+
+ var router = () => {
+    if (location.hash == '#speakers-list') {
+    // TODO afficher vue liste des présentateurs
+    } else if (location.hash == '#sessions-list') {
+    // TODO afficher vue liste des sessions
+    } else {
+    // TODO afficher vue par défaut
+    }
+    }
+    window.addEventListener('load', () => {
+    window.onhashchange = () => {
+    router();
+    };
+    router();
+});
