fad42fa19f63720929d62aab5582391369773704
Ismail GACHA <igacha664@gmail.com>
2017-11-15 09:33:47 +0100

    delete all

--- src/defaultVue/index.js
+++
@@ -1,9 +0,0 @@
-import template from './defaultVue.html';
-
-export default class vueAccueil{
-
-    render(){
-        $("#main-view").html(template);
-    }
-
-}
--- src/layout/index.js
+++
@@ -1,9 +0,0 @@
-import template from './layout.html';
-
-export default class layout{
-
-    render(){
-        $("body").html(template);
-    }
-
-}
